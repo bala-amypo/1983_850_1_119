b6fc7f95d55dbfb637a9a1a71e2b17dbd722e538
bala-amypo <bala@amypo.in>
2025-12-22 09:47:24 +0000

    User commit

--- src/main/java/com/example/demo/entity/Stock.java
+++ src/main/java/com/example/demo/entity/Stock.java
@@ -1,42 +1,15 @@
-package com.example.demo.entity;
-
-import jakarta.persistence.*;
-
 @Entity
+@Table(name = "stocks", uniqueConstraints = @UniqueConstraint(columnNames = "ticker"))
 public class Stock {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
     private String ticker;
-
     private String companyName;
     private String sector;
     private Boolean active = true;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTicker() {
-        return ticker;
-    }
-
-    public void setTicker(String ticker) {
-        this.ticker = ticker;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
+    // getters & setters
 }
