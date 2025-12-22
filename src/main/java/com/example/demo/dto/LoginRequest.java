5fb76831ecf1f7e78365d1a6d9399da95ad085bb
bala-amypo <bala@amypo.in>
2025-12-22 10:08:24 +0000

    User commit

--- src/main/java/com/example/demo/dto/LoginRequest.java
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -1,20 +1,8 @@
-package com.example.demo.model;
+package com.example.demo.dto;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "users")
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+public class LoginRequest {
     private String username;
     private String password;
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
 
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
