ba7d17bc4d979af62e28c8ab474611d836cc97b7
bala-amypo <bala@amypo.in>
2025-12-23 04:28:13 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -14,26 +14,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9189.pro604cr.amypo.ai/")
+                        new Server().url("https://9283.408procr.amypo.ai/)
                 ));
         }
 }
-
-// package com.example.demo.config;
-
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.info.Info;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-
-// @Configuration
-// public class OpenApiConfig {
-
-//     @Bean
-//     public OpenAPI openAPI() {
-//         return new OpenAPI()
-//                 .info(new Info()
-//                         .title("Stock Portfolio Risk Analyzer")
-//                         .version("1.0"));
-//     }
-// }
