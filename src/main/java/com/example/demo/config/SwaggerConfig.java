ac646b237f7935c03e60950e399659b3be8f74cf
bala-amypo <bala@amypo.in>
2025-12-23 05:25:14 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,29 +1,29 @@
-// package com.example.demo.config;
+package com.example.demo.config;
 
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.info.Info;
-// import io.swagger.v3.oas.models.security.SecurityRequirement;
-// import io.swagger.v3.oas.models.security.SecurityScheme;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// @Configuration
-// public class SwaggerConfig {
+@Configuration
+public class SwaggerConfig {
 
-//     @Bean
-//     public OpenAPI customOpenAPI() {
+    @Bean
+    public OpenAPI customOpenAPI() {
 
-//         return new OpenAPI()
-//                 .info(new Info()
-//                         .title("Portfolio Risk Analysis API")
-//                         .version("1.0")
-//                         .description("Swagger UI for Portfolio Risk Analysis System"))
-//                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-//                 .components(new io.swagger.v3.oas.models.Components()
-//                         .addSecuritySchemes("bearerAuth",
-//                                 new SecurityScheme()
-//                                         .type(SecurityScheme.Type.HTTP)
-//                                         .scheme("bearer")
-//                                         .bearerFormat("JWT")));
-//     }
-// }
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Portfolio Risk Analysis API")
+                        .version("1.0")
+                        .description("Swagger UI for Portfolio Risk Analysis System"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
